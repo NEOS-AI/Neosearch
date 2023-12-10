225c3639140aac0752c3b7f4281873d5f6404997
YeonwooSung <neos960518@gmail.com>
2023-12-10 21:18:10 +0900

    Solve code linter level warnings

--- src/neosearch/api/search.py
+++ src/neosearch/api/search.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Response, status
+from fastapi import APIRouter, Request, Response
 
 
 api = APIRouter()
--- src/neosearch/main.py
+++ src/neosearch/main.py
@@ -11,17 +11,25 @@ from neosearch.utils import Logger  # noqa: E402
 
 # init fastapi
 app = init_app(use_rate_limitter=True)
+logger = Logger()
 
 # start up event
 @app.on_event("startup")
 async def startup_event() -> None:
-    Logger().get_logger()  # init logger before app starts up
+    logger.get_logger()  # init logger before app starts up
     init_sbert_ray_serve()  # init ray serve
 
 # shutdown event
 @app.on_event("shutdown")
 async def shutdown_event() -> None:
     pass
+
+
+@app.get("/")
+async def root(request: Request) -> dict:
+    req_id = request.state.request_id
+    logger.log_debug(f"Request ID: {req_id} :: Request Root")
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
