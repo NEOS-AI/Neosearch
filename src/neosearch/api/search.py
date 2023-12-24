b2e284f90a4fa606fd86dfc6ba0e6d3f567f87ca
YeonwooSung <neos960518@gmail.com>
2023-12-24 18:05:41 +0900

    feat: organize todo list for basic search

--- src/neosearch/api/search.py
+++ src/neosearch/api/search.py
@@ -5,4 +5,7 @@ api = APIRouter()
 
 @api.get("/search")
 async def search(request: Request) -> Response:
+    #TODO validate request
+    #TODO execute search (request to Ray Serve)
+    #TODO return response
     return {"message": "Hello World"}
