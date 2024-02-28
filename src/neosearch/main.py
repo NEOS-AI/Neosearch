2d03548fda9ea0fa93200baf7c59291a98f1fc69
YeonwooSung <neos960518@gmail.com>
2024-02-28 13:22:54 +0900

    refactor: Update the project with create-llama

--- src/neosearch/main.py
+++ src/neosearch/main.py
@@ -1,51 +1,36 @@
-from fastapi import Request
-import sys
-from ray import serve
+from dotenv import load_dotenv
 
-# configure the directory name functions as a package
-sys.path.append("..")
+load_dotenv()
 
-# custom modules
-from neosearch.ai.sbert import sbert_app  # noqa: E402
-from neosearch.app import init_app  # noqa: E402
-from neosearch.utils import Logger  # noqa: E402
-
-# init fastapi
-app = init_app(use_rate_limitter=True)
-logger = Logger()
-
-# start up event
-@app.on_event("startup")
-async def startup_event() -> None:
-    logger.get_logger()  # init logger before app starts up
-
-    #TODO find way to run ray serve with custom grpc options
-
-    # start ray serve with custom http options
-    serve.start(http_options={"host": "0.0.0.0", "port": 10518})
-
-    # deploy sbert app
-    serve.create_backend("sbert", sbert_app)
-    serve.create_endpoint("sbert", backend="sbert")
+import logging
+import os
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.routers.chat import chat_router
+from app.settings import init_settings
 
 
-# shutdown event
-@app.on_event("shutdown")
-async def shutdown_event() -> None:
-    pass
+app = FastAPI()
+
+init_settings()
+
+environment = os.getenv("ENVIRONMENT", "dev")  # Default to 'development' if not set
 
 
-@app.get("/")
-async def root(request: Request) -> dict:
-    req_id = request.state.request_id
-    logger.log_debug(f"Request ID: {req_id} :: Request Root")
-    return {"message": "Hello World"}
+if environment == "dev":
+    logger = logging.getLogger("uvicorn")
+    logger.warning("Running in development mode - allowing CORS for all origins")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(chat_router, prefix="/api/chat")
 
 
 if __name__ == "__main__":
-    try:
-        import uvicorn
-
-        uvicorn.run("main:app", host="0.0.0.0", port=8518, log_level="warning", reload=True)
-    except ImportError:
-        print("Uvicorn is not installed. Install it with `pip install uvicorn`.")
+    uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
