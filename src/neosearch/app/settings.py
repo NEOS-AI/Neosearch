8d421c65834a48498fe0fb7158053d2ac00dd595
YeonwooSung <neos960518@gmail.com>
2024-02-29 23:16:02 +0900

    feat: Update the embeddings settings

--- src/neosearch/app/settings.py
+++ src/neosearch/app/settings.py
@@ -1,10 +1,14 @@
 import os
 from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.settings import Settings
 
 
 def init_settings():
-    model = os.getenv("MODEL", "gpt-3.5-turbo")
+    model = os.getenv("MODEL", "gpt-4")
     Settings.llm = OpenAI(model=model)
+    Settings.embed_model = OpenAIEmbedding(
+        model="text-embedding-3-small", embed_batch_size=100
+    )
     Settings.chunk_size = 1024
     Settings.chunk_overlap = 20
