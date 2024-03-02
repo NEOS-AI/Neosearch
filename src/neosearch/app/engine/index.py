b1c80aca9e83ea0dfbe929b2d785ad8171da1c16
YeonwooSung <neos960518@gmail.com>
2024-03-02 15:18:20 +0900

    feat: make vector store container as singleton for DB efficiency

--- src/neosearch/app/engine/index.py
+++ src/neosearch/app/engine/index.py
@@ -1,13 +1,14 @@
-import logging
 from llama_index.core.indices.vector_store import VectorStoreIndex
+
+# custom module
 from neosearch.app.engine.utils import init_pg_vector_store_from_env
+from neosearch.app.utils.logging import Logger
 
-logger = logging.getLogger("uvicorn")
-
+logger = Logger()
 
 def get_index():
-    logger.info("Connecting to index from PGVector...")
+    logger.log_info("Connecting to index from PGVector...")
     store = init_pg_vector_store_from_env()
-    index = VectorStoreIndex.from_vector_store(store)
-    logger.info("Finished connecting to index from PGVector.")
+    index = VectorStoreIndex.from_vector_store(store, use_async=True)
+    logger.log_info("Finished connecting to index from PGVector.")
     return index
