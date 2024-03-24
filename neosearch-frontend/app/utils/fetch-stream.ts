ddd859cfc53a37f128b66fb54bcf64be6ee67891
YeonwooSung <neos960518@gmail.com>
2024-03-24 15:10:27 +0900

    docs: Document the response streaming

--- neosearch-frontend/app/utils/fetch-stream.ts
+++ neosearch-frontend/app/utils/fetch-stream.ts
@@ -1,3 +1,15 @@
+/**
+ * Pass the data from reader to controller.
+ * This function is called recursively until the reader is done.
+ * When the reader is done, the controller is closed.
+ * Otherwise, the data is passed to the controller and the function is called again.
+ *
+ * @param reader The reader to read data from.
+ * @param controller The controller to pass the data to.
+ * @param onChunk The callback to call when a chunk is read.
+ * @param onDone The callback to call when the reader is done.
+ * @returns A promise that resolves when the reader is done.
+ */
 async function pump(
   reader: ReadableStreamDefaultReader<Uint8Array>,
   controller: ReadableStreamDefaultController,
@@ -13,7 +25,16 @@ async function pump(
   onChunk && onChunk(value);
   controller.enqueue(value);
   return pump(reader, controller, onChunk, onDone);
-}
+};
+
+/**
+ * Fetch a stream from the given response.
+ *
+ * @param response The response to fetch the stream from.
+ * @param onChunk The callback to call when a chunk is read.
+ * @param onDone The callback to call when the reader is done.
+ * @returns The stream.
+ */
 export const fetchStream = (
   response: Response,
   onChunk?: (chunk: Uint8Array) => void,
