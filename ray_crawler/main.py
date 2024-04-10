6337c6b0f0d07f4a5cc54375a24b55cbbd0ce0bf
YeonwooSung <neos960518@gmail.com>
2024-04-10 18:59:20 +0900

    feat: Enhance the deployment with batch embedding

--- ray_crawler/main.py
+++ ray_crawler/main.py
@@ -12,10 +12,16 @@ def url_crawl_test():
     url = "https://namu.wiki/w/%EC%95%84%EB%9D%BC%ED%95%98%EC%8B%9C%20%ED%83%80%EB%B9%84"
     print(extract_url_content(url))
 
-def main():
+def deploy_embedding_server():
     # Deploy the Ray Serve application.
     deployment = EmbeddingDeployment.bind()
     serve.run(deployment)
+
+def main(use_custom_server: bool = False):
+    if use_custom_server:
+        deploy_embedding_server()
+    #TODO crawl, extract, and index the content of the URL
+
 
 if __name__ == "__main__":
     main()
