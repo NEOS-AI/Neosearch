acea8e53f4797f7a0390d75f868053f4ce42bfc2
YeonwooSung <neos960518@gmail.com>
2024-04-23 19:16:16 +0900

    feat: Start implementing RAG search with external search engines

---
+++ neosearch/app/rag.py
@@ -0,0 +1,67 @@
+import os
+import httpx
+
+# custom import
+from neosearch.api.utils.rag_search import (
+    search_with_bing,
+    search_with_google,
+    search_with_serper,
+    search_with_searchapi,
+)
+from neosearch.constants.rag_search import RAG_SEARCH_HANDLER_MAX_CONCURRENCY
+from neosearch.utils.singleton import Singleton
+
+
+def get_rag_searcher() -> "RagSearcher":
+    """Get the RAG searcher."""
+    return RagSearcher()
+
+
+class RagSearcher(metaclass=Singleton):
+    def __init__(
+        self,
+        backend: str = "SEARCHAPI",
+        max_concurrency: int = RAG_SEARCH_HANDLER_MAX_CONCURRENCY,
+    ) -> None:
+        self.backend = backend
+        self.max_concurrency = max_concurrency
+
+    def init(self):
+        if self.backend == "SEARCHAPI":
+            self.search_api_key = os.environ["SEARCHAPI_API_KEY"]
+            self.search_function = lambda query: search_with_searchapi(
+                query,
+                self.search_api_key,
+            )
+        elif self.backend == "LEPTON":
+            from lepton import Client  # type: ignore
+            from leptonai.api.workspace import WorkspaceInfoLocalRecord  # type: ignore
+
+            self.leptonsearch_client = Client(
+                "https://search-api.lepton.run/",
+                token=os.environ.get("LEPTON_WORKSPACE_TOKEN")
+                or WorkspaceInfoLocalRecord.get_current_workspace_token(),
+                stream=True,
+                timeout=httpx.Timeout(connect=10, read=120, write=120, pool=10),
+            )
+        elif self.backend == "BING":
+            self.search_api_key = os.environ["BING_SEARCH_V7_SUBSCRIPTION_KEY"]
+            self.search_function = lambda query: search_with_bing(
+                query,
+                self.search_api_key,
+            )
+        elif self.backend == "GOOGLE":
+            self.search_api_key = os.environ["GOOGLE_SEARCH_API_KEY"]
+            self.search_function = lambda query: search_with_google(
+                query,
+                self.search_api_key,
+                os.environ["GOOGLE_SEARCH_CX"],
+            )
+        elif self.backend == "SERPER":
+            self.search_api_key = os.environ["SERPER_SEARCH_API_KEY"]
+            self.search_function = lambda query: search_with_serper(
+                query,
+                self.search_api_key,
+            )
+        else:
+            raise RuntimeError("Backend must be LEPTON, BING, GOOGLE, SERPER or SEARCHAPI.")
