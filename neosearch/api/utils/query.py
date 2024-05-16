718ba28c24571b889a008187120ab16dd826be21
YeonwooSung <neos960518@gmail.com>
2024-05-16 22:10:20 +0900

    refactor: it is not good to ignore linter warnings..

--- neosearch/api/utils/query.py
+++ neosearch/api/utils/query.py
@@ -7,5 +7,7 @@ from neosearch.models.query_models import QueryData
 async def validate_query_data(data: QueryData):
     query_data = data.query
     if query_data is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No query provided",)  # noqa: E501
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No query provided",
+        )
     return query_data
