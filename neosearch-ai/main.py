68dbb1bc22908701041096eb615baf50633e2c56
YeonwooSung <neos960518@gmail.com>
2024-08-03 23:40:09 +0900

    feat: Implement skeleton server with embedding engine

---
+++ neosearch-ai/main.py
@@ -0,0 +1,49 @@
+import sys
+import ray
+from ray import serve
+import torch
+
+sys.path.append("..")
+
+# custom modules
+from engine.embeddings import EmbeddingDeployment
+
+
+def deploy_embedding_server():
+    # Deploy the Ray Serve application.
+    deployment = EmbeddingDeployment.bind()
+    serve.run(
+        deployment,
+        blocking=False,
+        name="embedding_server",
+        route_prefix="/embed"
+    )
+
+
+def main(
+    use_custom_server: bool = True,
+    num_cpus: int = 2,
+    num_gpus: int = 0
+) -> None:
+    if use_custom_server:
+        # Set PyTorch internal threads to 1 to avoid thread contention.
+        torch.set_num_threads(1)
+
+        # init ray with the custom configuration
+        ray.init(
+            num_cpus=num_cpus,
+            num_gpus=num_gpus,
+        )
+
+        deploy_embedding_server()
+    #TODO crawl, extract, and index the content of the URL
+
+
+if __name__ == "__main__":
+    cuda_available = torch.cuda.is_available()
+    if cuda_available:
+        # get num of gpus
+        num_gpus = torch.cuda.device_count()
+        main(num_gpus=num_gpus)
+    else:
+        main()
