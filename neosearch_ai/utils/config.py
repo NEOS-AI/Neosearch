14a0b1fd3922c0c7a53acd01ff9f6ad608def3f5
YeonwooSung <neos960518@gmail.com>
2024-10-12 02:36:16 +0900

    refactor: Make config dataclass to store configurations

---
+++ neosearch_ai/utils/config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+import torch
+import multiprocessing
+
+
+@dataclass
+class NeosAiConfig:
+    num_of_cpus: int = multiprocessing.cpu_count()
+    cuda_available: bool = torch.cuda.is_available()
+    use_llm2vec: bool = False
+    avoid_thread_contention: bool = True
