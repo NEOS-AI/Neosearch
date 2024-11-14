bcbb5bd6cce4f28a4e9110fec3cbb773ed1a2eca
YeonwooSung <neos960518@gmail.com>
2024-11-14 23:52:30 +0900

    cleanup unnecessary files

--- sql/paradedb/run_with_docker.sh
+++
@@ -1,2 +0,0 @@
-# start a ParadeDB instance with default user postgres and password password
-sudo docker run --name paradedb -e POSTGRES_PASSWORD=password paradedb/paradedb
--- sql/psql/ts_config.sql
+++
@@ -1,5 +0,0 @@
--- Admin query to retrieve all available text search configurations
-\dF
-
--- Check all available text search configurations
-SELECT cfgname FROM pg_ts_config;
