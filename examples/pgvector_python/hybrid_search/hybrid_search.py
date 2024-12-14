0e5fbe8061b21f63d04cca2ff07ac09d12b5df02
YeonwooSung <neos960518@gmail.com>
2024-12-14 20:07:05 +0900

    refactor: Update hybrid search codebase

--- examples/pgvector_python/hybrid_search/hybrid_search.py
+++ examples/pgvector_python/hybrid_search/hybrid_search.py
@@ -1,7 +1,11 @@
 from pgvector.psycopg import register_vector
 import psycopg
 from sentence_transformers import SentenceTransformer
+from datasets import load_dataset
 
+
+# Load the dataset
+ds = load_dataset("rag-datasets/rag-mini-wikipedia", "text-corpus")
 
 conn = psycopg.connect(dbname='pgvector_example', autocommit=True)
 
@@ -12,11 +16,10 @@ conn.execute('DROP TABLE IF EXISTS documents')
 conn.execute('CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding vector(384))')
 conn.execute("CREATE INDEX ON documents USING GIN (to_tsvector('english', content))")
 
-sentences = [
-    'The dog is barking',
-    'The cat is purring',
-    'The bear is growling'
-]
+sentences = ds['passages']
+# sentences to list of str
+sentences = [s['passage'] for s in sentences]
+
 model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 embeddings = model.encode(sentences)
 for content, embedding in zip(sentences, embeddings):
@@ -25,13 +28,13 @@ for content, embedding in zip(sentences, embeddings):
 
 sql = """
 WITH semantic_search AS (
-    SELECT id, RANK () OVER (ORDER BY embedding <=> %(embedding)s) AS rank
+    SELECT id, RANK () OVER (ORDER BY embedding <=> %(embedding)s) AS rank, content
     FROM documents
     ORDER BY embedding <=> %(embedding)s
     LIMIT 20
 ),
 keyword_search AS (
-    SELECT id, RANK () OVER (ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC)
+    SELECT id, RANK () OVER (ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC), content
     FROM documents, plainto_tsquery('english', %(query)s) query
     WHERE to_tsvector('english', content) @@ query
     ORDER BY ts_rank_cd(to_tsvector('english', content), query) DESC
@@ -40,7 +43,8 @@ keyword_search AS (
 SELECT
     COALESCE(semantic_search.id, keyword_search.id) AS id,
     COALESCE(1.0 / (%(k)s + semantic_search.rank), 0.0) +
-    COALESCE(1.0 / (%(k)s + keyword_search.rank), 0.0) AS score
+    COALESCE(1.0 / (%(k)s + keyword_search.rank), 0.0) AS score,
+    COALESCE(semantic_search.content, keyword_search.content) AS content
 FROM semantic_search
 FULL OUTER JOIN keyword_search ON semantic_search.id = keyword_search.id
 ORDER BY score DESC
@@ -51,4 +55,8 @@ embedding = model.encode(query)
 k = 60
 results = conn.execute(sql, {'query': query, 'embedding': embedding, 'k': k}).fetchall()
 for row in results:
-    print('document:', row[0], 'RRF score:', row[1])
+    print('document:', row[0], 'RRF score:', row[1], 'content:', row[2])
+
+
+# Close the connection
+conn.close()
