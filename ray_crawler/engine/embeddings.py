c5b23ff887f6d9770755ed699bfff8d549754a68
YeonwooSung <neos960518@gmail.com>
2024-12-22 17:01:58 +0900

    refactor: Clean up unnecessary engine codebases

--- ray_crawler/engine/embeddings.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
-# from llama_index.embeddings import LangchainEmbedding
-
-
-def get_embedding_model(model_name: str = "sentence-transformers/all-mpnet-base-v2"):
-    # return LangchainEmbedding(HuggingFaceEmbeddings(model_name=model_name))
-    return HuggingFaceEmbeddings(model_name=model_name)
