f2c8a1b8143a52155f713bba8483b47f8e3116ef
YeonwooSung <neos960518@gmail.com>
2024-12-26 19:32:45 +0900

    feat: Update trafilatura utils

--- ray_crawler/utils/trafilatura_util.py
+++ ray_crawler/utils/trafilatura_util.py
@@ -2,14 +2,23 @@ import trafilatura
 from typing import Union
 
 
-def extract_url_content(url, output_format: Union[str, None] = "markdown"):
+def extract_url_content(url, output_format: Union[str, None] = "markdown", include_tables: bool = True, deduplicate: bool = False):
     if output_format not in {"json", "xml", "markdown"}:
         output_format = None
 
     downloaded = trafilatura.fetch_url(url)
 
     if output_format is None:
-        content =  trafilatura.extract(downloaded)
+        content =  trafilatura.extract(
+            downloaded,
+            include_tables=include_tables,
+            deduplicate=deduplicate
+        )
     else:
-        content = trafilatura.extract(downloaded, output_format=output_format)
+        content = trafilatura.extract(
+            downloaded,
+            deduplicate=deduplicate,
+            output_format=output_format,
+            include_tables=include_tables
+        )
     return {"url":url, "content":content}
