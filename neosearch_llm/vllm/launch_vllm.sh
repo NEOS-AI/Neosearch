b3ea25c4b44a0cfe9ece053c74adcabdbca6d814
YeonwooSung <neos960518@gmail.com>
2024-12-28 15:28:42 +0900

    udpate the vllm launch script

--- neosearch_llm/vllm/launch_vllm.sh
+++ neosearch_llm/vllm/launch_vllm.sh
@@ -3,4 +3,5 @@ pip install --upgrade pip
 sh export_requirements_from_poetry.sh
 pip install -r requirements.txt
 
-vllm serve meta-llama/Meta-Llama-3.1-8B
+# For passing the API key, you could use it for authentication with OpenAI python client
+vllm serve NousResearch/Meta-Llama-3-8B-Instruct --dtype auto --api-key nsk-myapikey
