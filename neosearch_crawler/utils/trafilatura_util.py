dc59568fcb73973698ec3c782839b17a34a3d5fb
YeonwooSung <neos960518@gmail.com>
2025-02-23 21:55:23 +0900

    feat: Update trafilatura utils to add web spider features

--- neosearch_crawler/utils/trafilatura_util.py
+++ neosearch_crawler/utils/trafilatura_util.py
@@ -1,8 +1,63 @@
+from copy import deepcopy
 import trafilatura
+from trafilatura.settings import DEFAULT_CONFIG
+from trafilatura.spider import focused_crawler, is_still_navigation
 from typing import Union
 
 
-def extract_url_content(url, output_format: Union[str, None] = "markdown", include_tables: bool = True, deduplicate: bool = False):
+def init_trafilatura_config():
+    my_config = deepcopy(DEFAULT_CONFIG)
+
+    #TODO see more options in <https://trafilatura.readthedocs.io/en/latest/settings.html>
+
+    return my_config
+
+
+def run_focused_crawler(
+    url: str,
+    max_seen_urls: int = 10,
+    max_known_urls: int = 100000,
+    todo: Union[list, None] = None,
+    known_links: Union[list, None] = None,
+    lang: Union[str, None] = None
+) -> tuple[list[str], list[str], bool]:
+    """
+
+    Args:
+        max_seen_urls: the maximum number of pages to visit (default: 10)
+        max_known_urls: the maximum number of pages to “know” about (default: 100000)
+        todo: provide a previously generated list of pages to visit (i.e. a crawl frontier)
+        known_links: provide a list of previously known pages
+        lang: try to target links according to language heuristics (two-letter code)
+
+    Returns:
+        tuple: Returns the snapshot of the current state of the crawler.
+        - to_visit: the list of pages to visit (next visited)
+        - known_links: the list of known (list of know links)
+        - navigatable: whether the crawler is still navigatable
+    """
+    # `to_visit` variable keeps track of what is ahead
+    # `known_links` variable ensures that the same pages are not visited twice
+    to_visit, known_links = focused_crawler(
+        url,
+        max_seen_urls=max_seen_urls,
+        max_known_urls=max_known_urls,
+        todo=todo,
+        known_links=known_links,
+        lang=lang,
+    )
+
+    navigatable = is_still_navigation(to_visit)
+
+    return to_visit, known_links, navigatable
+
+
+def extract_url_content(
+    url,
+    output_format: Union[str, None] = "markdown",
+    include_tables: bool = True,
+    deduplicate: bool = False
+):
     if output_format not in {"json", "xml", "markdown"}:
         output_format = None
 
